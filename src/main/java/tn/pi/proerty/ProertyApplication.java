5260145a98967d20672932c8d13c83a46c3e080f
ski1a <bilelzr8@gmail.com>
2024-03-12 23:44:36 +0100

    first commit

---
+++ src/main/java/tn/pi/proerty/ProertyApplication.java
@@ -0,0 +1,13 @@
+package tn.pi.proerty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProertyApplication.class, args);
+    }
+
+}
